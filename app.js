d313c5b05eb114a9b72c064bd0ecf7a8105dd06c
Renato Delboni <renatonato_jr@hotmail.com>
2022-01-22 16:08:53 +0900

    devtools console logging

--- app.js
+++ app.js
@@ -1,33 +1,43 @@
 const dogs = [{ name: 'Snickers', age: 2 }, { name: 'hugo', age: 8 }];
 
-    function makeGreen() {
-      const p = document.querySelector('p');
-      p.style.color = '#BADA55';
-      p.style.fontSize = '50px';
-    }
+function makeGreen() {
+  const p = document.querySelector('p');
+  p.style.color = '#BADA55';
+  p.style.fontSize = '50px';
+}
 
-    // Regular
+// Regular
+console.log('Hello');
 
-    // Interpolated
+// Interpolated
+console.log("Hello, I am %s thing", 'some');
+//can also use backticks for this
 
-    // Styled
-
-    // warning!
-
-    // Error :|
-
-    // Info
-
-    // Testing
-
-    // clearing
-
-    // Viewing DOM Elements
-
-    // Grouping together
-
-    // counting
-
-    // timing
+// Styled
 
 
+// warning!
+
+
+// Error :|
+
+
+// Info
+
+
+// Testing
+
+
+// clearing
+
+
+// Viewing DOM Elements
+
+
+// Grouping together
+
+
+// counting
+
+
+// timing
